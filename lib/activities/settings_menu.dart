f22013839c57347b305fe398bb5a6182d61eef05
Patrick Coral <patrick.coral@epfl.ch>
2022-06-09 18:06:20 +0200

    Settings dropdown menus

--- lib/activities/settings_menu.dart
+++ lib/activities/settings_menu.dart
@@ -12,6 +12,20 @@ class SettingsMenu extends StatefulWidget {
 class _SettingsMenuState extends State<SettingsMenu> {
   List<String> amoutQ = ["3", "4", "5", "6", "7", "8", "9", "10"];
   List<String> difficulty = ["easy", "medium", "hard"];
+  var _amountDropdownValue = "6";
+  var _diffDropdownValue = "medium";
+
+  void amountDropdownCallback(String? selectedValue) {
+    if (selectedValue is String) {
+      setState(() => _amountDropdownValue = selectedValue);
+    }
+  }
+
+  void diffDropdownCallback(String? selectedValue) {
+    if (selectedValue is String) {
+      setState(() => _diffDropdownValue = selectedValue);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +33,35 @@ class _SettingsMenuState extends State<SettingsMenu> {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: const Center(child: Text("hello")),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Center(
+          child: Column(children: [
+            const Text("Amount of questions per card:"),
+            DropdownButton<String>(
+              items: amoutQ
+                  .map((e) => DropdownMenuItem(
+                        value: e,
+                        child: Text(e),
+                      ))
+                  .toList(),
+              value: _amountDropdownValue,
+              onChanged: amountDropdownCallback,
+            ),
+            const Text("Question difficulty:"),
+            DropdownButton<String>(
+              items: difficulty
+                  .map((e) => DropdownMenuItem(
+                        value: e,
+                        child: Text(e),
+                      ))
+                  .toList(),
+              value: _diffDropdownValue,
+              onChanged: diffDropdownCallback,
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
